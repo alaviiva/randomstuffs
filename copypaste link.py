a9ec5ca1ce1758e0f5a901791e1ca045c47e75e5
Sampo Vartiala <sampo.vartiala@gmail.com>
2018-02-19 15:49:34 +0200

    Fix link pastin

--- copypaste link.py
+++ copypaste link.py
@@ -5,6 +5,7 @@ import time
 #if window.wait_for_focus(".*Firefox.*", 0):
 if window.get_active_class().endswith("Firefox"):
   keyboard.send_keys("<ctrl>+l")
+  time.sleep(0.1)
   keyboard.send_keys("<ctrl>+c")
 
 window.activate("Terminal")
